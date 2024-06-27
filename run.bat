d7814fdd199a2ed85ab0ae306d79b40be73ffd3b
SotchNam <sotchnam@gmail.com>
2024-06-27 18:52:02 +0100

    Added .h copy script for windows weaklings

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+copy src\motors.h tests\motorTest\motors.h
+copy src\pins.h tests\buzzTest\buzzer.h
+copy src\pins.h tests\motorTest\motors.h
+copy src\pins.h tests\qtrTest\buzzer.h
+copy src\pins.h tests\qtrTest\motors.h
+copy src\qtr.h tests\irs\qtr.h
+copy src\qtr.h tests\qtrTest\qtr.h
+copy src\tcs.h tests\colorTest\tcs.h
+copy src\tcs.h tests\freertos\tcs.h

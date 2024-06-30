3ce88e4621a44a3eeb73ad0533d9804e85aefcf6
SotchNam <sotchnam@gmail.com>
2024-06-30 19:33:23 +0100

    python error

--- src/motors.h
+++ src/motors.h
@@ -38,7 +38,7 @@ void left(int speed1, int speed2) {
 
 // allows bidirectional control from one function
 void setMotors(int speed1, int speed2) {
-  if (speed1 > 0) { # left
+  if (speed1 > 0) { // left
     digitalWrite(motor1, 1);
     digitalWrite(motor2, 0);
   } else {
@@ -47,7 +47,7 @@ void setMotors(int speed1, int speed2) {
   }
   analogWrite(motor1Speed, abs(speed1));
 
-  if (speed2 > 0) { # right
+  if (speed2 > 0) { // right
     digitalWrite(motor3, 1);
     digitalWrite(motor4, 0);
   } else {

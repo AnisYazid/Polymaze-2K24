c6d9e2734e4b6edddd9e506aebed05a5fd231d24
SotchNam <sotchnam@gmail.com>
2024-06-30 16:13:05 +0100

    Corrected the headers copy script

--- run.bat
+++ run.bat
@@ -1,11 +1,12 @@
 @echo off
 
-copy src\motors.h tests\motorTest\motors.h
-copy src\pins.h tests\buzzTest\buzzer.h
-copy src\pins.h tests\motorTest\motors.h
-copy src\pins.h tests\qtrTest\buzzer.h
-copy src\pins.h tests\qtrTest\motors.h
-copy src\qtr.h tests\irs\qtr.h
-copy src\qtr.h tests\qtrTest\qtr.h
-copy src\tcs.h tests\colorTest\tcs.h
-copy src\tcs.h tests\freertos\tcs.h
+copy /Y src\motors.h tests\motorTest\motors.h
+copy /Y src\motors.h tests\qtrTest\motors.h
+copy /Y src\pins.h tests\motorTest\pins.h
+copy /Y src\pins.h tests\qtrTest\pins.h
+copy /Y src\buzzer.h tests\qtrTest\buzzer.h
+copy /Y src\buzzer.h tests\buzzTest\buzzer.h
+copy /Y src\qtr.h tests\irs\qtr.h
+copy /Y src\qtr.h tests\qtrTest\qtr.h
+copy /Y src\tcs.h tests\colorTest\tcs.h
+copy /Y src\tcs.h tests\freertos\tcs.h
